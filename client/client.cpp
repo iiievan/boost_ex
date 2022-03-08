43269e7683cb81c95bcdfc819690c83437f7dca5
mbo <mbo.kastnev@gmail.com>
2022-03-08 21:09:32 +0300

    Модифицировал makefile's а так же сами исходники для клиента и сервера. Чтобы можно было компилировать как на компьютере так и на "Малине".

--- client/client.cpp
+++ client/client.cpp
@@ -1,12 +1,12 @@
 /** на стороне клиента
-сборка: 
-g++ -pthread -lboost_system client.cpp -o client
 использование: 
 ./client 192.168.0.94 2001 hello
 **/
 
 #include <iostream>
 #include <boost/asio.hpp>
+
+using namespace boost;
 
 int main(int argc, char* argv[])
 {
@@ -16,16 +16,16 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    auto const address = boost::asio::ip::make_address(argv[1]);
+    auto const address = asio::ip::make_address(argv[1]);
     auto const port = std::atoi(argv[2]);
     std::string msg = argv[3];
     msg = msg + '\n';
 
-    boost::asio::io_service io;
-    boost::asio::ip::tcp::socket sock(io);
-    boost::system::error_code ec;
+    asio::io_service io;
+    asio::ip::tcp::socket sock(io);
+    system::error_code ec;
 
-    sock.connect(boost::asio::ip::tcp::endpoint(address, port), ec);
+    sock.connect(asio::ip::tcp::endpoint(address, port), ec);
 
     if(ec)
     {
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    boost::asio::write(sock, boost::asio::buffer(msg), ec);
+    asio::write(sock, asio::buffer(msg), ec);
 
     if (ec)
     {
@@ -41,16 +41,16 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    boost::asio::streambuf receive_buffer;
-    boost::asio::read(sock,receive_buffer,boost::asio::transfer_all(), ec);
+    asio::streambuf receive_buffer;
+    asio::read(sock,receive_buffer,asio::transfer_all(), ec);
 
-    if(ec && ec != boost::asio::error::eof)
+    if(ec && ec != asio::error::eof)
     {
         std::cout<< "receive failed " << ec.message() << std::endl;
     }
     else
     {
-        const char * data = boost::asio::buffer_cast<const char *>(receive_buffer.data());
+        const char * data = asio::buffer_cast<const char *>(receive_buffer.data());
         std::cout << data << std::endl;
     }
 
--- server/server.cpp
+++ server/server.cpp
@@ -1,16 +1,12 @@
 /** на стороне сервера - это Raspberry pi
-сборка: 
-g++ -pthread -lboost_system server.cpp -o server
 использование: 
 ./server 192.168.0.94 2001
 **/
 
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
-// using namespace boost::asio
-// using tcp::ip
+using namespace boost;
 
 int main(int argc, char* argv[])
 {
@@ -19,18 +15,18 @@ int main(int argc, char* argv[])
         std::cout << "Wrong parameters\n" << "Example usage 192.168.0.1 2001" << std::endl;
     }
 
-    auto const address = boost::asio::ip::make_address(argv[1]);
+    auto const address = asio::ip::make_address(argv[1]);
     auto const port = std::atoi(argv[2]);
 
-    boost::asio::io_service io;
-    boost::system::error_code ec;
-    boost::asio::ip::tcp::acceptor acceptor_(io, boost::asio::ip::tcp::endpoint(address, port));
-    boost::asio::ip::tcp::socket socket_(io);
+    asio::io_service ios;
+    system::error_code ec;
+    asio::ip::tcp::acceptor acc(ios, asio::ip::tcp::endpoint(address, port));
+    asio::ip::tcp::socket sock(ios);
+    asio::streambuf buf;
 
-    acceptor_.accept(socket_);
+    acc.accept(sock);
 
-    boost::asio::streambuf buf;
-    boost::asio::read_until(socket_, buf, "\n", ec);
+    asio::read_until(sock, buf, "\n", ec);
 
     if(ec)
     {
@@ -38,10 +34,10 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+    std::string data = asio::buffer_cast<const char*>(buf.data());
     std::cout << data << std::endl;
 
-    boost::asio::write(socket_, boost::asio::buffer(data), ec);
+    asio::write(sock, asio::buffer(data), ec);
 
     if (ec)
     {
